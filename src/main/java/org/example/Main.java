d9315a342b2ed6ebf054298d3779d2ef3bdcbb48
Nicodawdev <napc67@educa.madrid.org>
2026-04-06 22:51:33 +0200

    Actualizada la cabecera principal de la aplicación

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         GestorTareas gestor = new GestorTareas();
-        System.out.println("=== GESTOR DE TAREAS ===");
+        System.out.println("=== ORGANIZADOR PERSONAL DE TAREAS ===");
         gestor.listarTareas();
     }
 }
